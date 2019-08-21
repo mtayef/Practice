c4b8a1da010d54bab001b32323f79231a8455e7a
mtayef <30615194+mtayef@users.noreply.github.com>
2019-08-21 09:45:55 -0400

    Create Screenshot.java
    Will change the package to robotStuff

---
+++ JSonJava/src/test/java/newStuff/Screenshot.java
@@ -0,0 +1,35 @@
+//https://www.geeksforgeeks.org/java-program-take-screenshots/
+// Java Program to Capture full 
+// Image of Screen 
+import java.awt.AWTException; 
+import java.awt.Rectangle; 
+import java.awt.Toolkit; 
+import java.awt.Robot; 
+import java.awt.image.BufferedImage; 
+import java.io.IOException; 
+import java.io.File; 
+import javax.imageio.ImageIO; 
+
+public class Screenshot { 
+	public static final long serialVersionUID = 1L; 
+	public static void main(String[] args) 
+	{ 
+		try { 
+			Thread.sleep(120); 
+			Robot r = new Robot(); 
+
+			// It saves screenshot to desired path 
+			String path = "U:// Shot.jpg"; 
+
+			// Used to get ScreenSize and capture image 
+			Rectangle capture = 
+			new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()); 
+			BufferedImage Image = r.createScreenCapture(capture); 
+			ImageIO.write(Image, "jpg", new File(path)); 
+			System.out.println("Screenshot saved"); 
+		} 
+		catch (AWTException | IOException | InterruptedException ex) { 
+			System.out.println(ex); 
+		} 
+	} 
+}
